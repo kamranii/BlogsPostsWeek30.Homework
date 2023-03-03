3e6887d9e13a4d2a4fad42e87797b90250e73683
kamranii <kamranii@code.edu.az>
2023-03-03 22:00:15 +0400

    logging added

--- Blog.Week30.Homework/Controllers/BlogController.cs
+++ Blog.Week30.Homework/Controllers/BlogController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Blog.Week30.Homework.Entities;
 using Blog.Week30.Homework.UnitOfWork;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions;
+using Serilog;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,10 +31,11 @@ namespace Blog.Week30.Homework.Controllers
         [HttpGet]
         public async Task<ActionResult<List<SBlog>>> Get()
         {
-            _logger.Log( LogLevel.Debug, "Listing all blog items");
+            _logger.Log( LogLevel.Information,$"Searching all blogs for request: {Request.Path}");
             var blogs = await _unitOfWork.blogRepository.GetAllList();
             if (blogs == null || blogs.Count == 0)
                 return NotFound("No blogs were found");
+            _logger.LogWarning($"Response generated to the request {Request.Path} is {JsonSerializer.Serialize(blogs)}");
             return blogs;
         }
 
@@ -40,10 +43,15 @@ namespace Blog.Week30.Homework.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<SBlog>> Get(int id)
         {
-            var book = await _unitOfWork.blogRepository.Find(id);
-            if (book == null)
+            _logger.LogWarning($"Request accepted to find the blog with id of {id} at {DateTime.Now}");
+            var blog = await _unitOfWork.blogRepository.Find(id);
+            if (blog == null)
+            {
+                _logger.LogError($"Attempt made to retrieve a non-existing blog at {DateTime.Now}");
                 return NotFound("No blog was found");
-            return book;
+            }
+            _logger.LogWarning($"Retrieved blog: {JsonSerializer.Serialize(blog)}");
+            return blog;
         }
 
         // POST api/values
@@ -56,12 +64,31 @@ namespace Blog.Week30.Homework.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            try { }
+            catch { }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            //log
+            _logger.LogCritical($"An attempt is made to delete a blog with id of" +
+                $"{id} at {DateTime.Now} by {User}");
+            //find
+            var blog = await _unitOfWork.blogRepository.Find(id);
+            //check
+            if(blog == null)
+            {
+                _logger.LogError($"No blog was found with id {id}. Unsuccessful delete operation " +
+                    $"at {DateTime.Now}");
+                return NotFound("No blog was found! Could not remove the element");
+            }
+            //remove
+            await _unitOfWork.blogRepository.Delete(blog);
+            await _unitOfWork.Commit();
+            _logger.LogInformation($"Blog {blog.Name} was deleted from Database at {DateTime.Now}");
+            return NoContent();
         }
     }
 }
--- Blog.Week30.Homework/Program.cs
+++ Blog.Week30.Homework/Program.cs
@@ -3,35 +3,61 @@ using Blog.Week30.Homework.Repository.Abstractions;
 using Blog.Week30.Homework.Repository.Implementations;
 using Blog.Week30.Homework.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+       System.IO.Path.Combine("/Users/kamranimranli/desktop", "LogFiles", "Application", "diagnostics.txt"),
+       rollingInterval: RollingInterval.Day,
+       fileSizeLimitBytes: 10 * 1024 * 1024,
+       retainedFileCountLimit: 2,
+       rollOnFileSizeLimit: true,
+       shared: true,
+       flushToDiskInterval: TimeSpan.FromSeconds(1))
+    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
-builder.Services.AddTransient<IBlogRepository, BlogRepository>();
-builder.Services.AddTransient<IPostRepository, PostRepository>();
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-builder.Services.AddDbContext<ApplicationDbContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog(); // <-- Add this line
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+    builder.Services.AddTransient<IBlogRepository, BlogRepository>();
+    builder.Services.AddTransient<IPostRepository, PostRepository>();
+    builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddDbContext<ApplicationDbContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
